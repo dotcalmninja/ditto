814b7ac955830a23641ad77fc0217d26d0ebd0fc
Pim Brouwers <mpbrouwers@gmail.com>
2018-01-22 07:34:25 -0500

    es6 & unit test conversion

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-/* Export Ditto */
+/*!
+ * ditto v1.x (https://github.com/pimbrouwers/ditto)
+ * Copyright 2018 Pim Brouwers
+ * Licensed under MIT (https://github.com/pimbrouwers/ditto/blob/master/LICENSE)
+ */
+
 module.exports = require('./src');
---
+++ spec/ditto.spec.js
@@ -0,0 +1,5 @@
+const DittoHbs = require('../src');
+
+describe('ditto', function() {
+
+});

463f9581b34706d4355b3cef6225dc2a403eda37
Pim Brouwers <pbrouwers@nhlpa.com>
2018-08-10 15:13:12 -0400

    path fixes again

--- src/dittoFile.js
+++ src/dittoFile.js
@@ -5,19 +5,29 @@ module.exports = DittoFile;
  @typedef DittoFile
  @type {Object}
  @property {Array} buffer file contents as byte array
- @property {String} relPath relative filepath
+ @property {Object} relPath relative filepath
  @property {Object} stats node Stats object
  */
 function DittoFile(buffer, relPath, stats) {
   this.content = buffer;
   this.stats = stats;
 
-  this.path = path.parse(relPath)  
-  
-  delete this.path.root;  
-  delete this.path.base;
+  this.path = {
+    dir: '', //directory(s) relative to source
+    ext: '', //file extension with dot
+    name: '', //file name without extension
+    rel: relPath // original path, relative to source with extension
+  };
+
+  var parsedPath = path.parse(relPath);
+  this.path.dir = parsedPath.dir;
+  this.path.ext = parsedPath.ext;
+  this.path.name = parsedPath.name;
 };
 
+/**
+ * Get path with extension for purpose of writing to disk
+ */
 DittoFile.prototype.pathWithExtension = function(){
   var pathWithExtension = path.join(this.path.dir, this.path.name + this.path.ext);
 

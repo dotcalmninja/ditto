364018f63e66e00ec0706eac34f0fad676bf7ea9
Pim Brouwers <pbrouwers@nhlpa.com>
2018-08-16 06:25:42 -0400

    makeRel

--- src/file.js
+++ src/file.js
@@ -15,12 +15,32 @@ function DittoFile(buffer, relPath, stats) {
   this.path = {
     dir: '', //directory(s) relative to source
     ext: '', //file extension with dot
-    name: '', //file name without extension
-    rel: path.normalize(relPath) // original path, relative to source with extension
+    name: '', //file name without extension    
+    rel: relPath //relative to source with extension
   };
 
   var parsedPath = path.parse(relPath);
   this.path.dir = parsedPath.dir;
   this.path.ext = parsedPath.ext;
   this.path.name = parsedPath.name;
+};
+
+
+function makeRel(){
+  this.path.rel = path.join(this.path.dir, this.path.name + this.path.ext);
+};
+
+DittoFile.prototype.setDir = function(dir){
+  this.path.dir = dir;  
+  makeRel.call(this);
+};
+
+DittoFile.prototype.setExt = function(ext){
+  this.path.ext = ext;  
+  makeRel.call(this);
+};
+
+DittoFile.prototype.setName = function(name){
+  this.path.name = name;  
+  makeRel.call(this);
 };
--- src/index.js
+++ src/index.js
@@ -140,7 +140,7 @@ Ditto.prototype.readFile = function (filepath, callback) {
 
     fs.readFile(filepath, function (err, buffer) {
       if (err) callback(err, null);
-
+      
       callback(null, new DittoFile(buffer, path.relative(self._source, filepath), stats));
     });
   })
